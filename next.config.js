0795fc52cb04a1ac1d3ddb657be32c6abe61b1c0
kingdayx <54965488+kingdayx@users.noreply.github.com>
2021-08-28 20:36:55 -0400

    updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  exportTrailingSlash: true,
+  exporPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
   images: {
     domains: ["links.papareact.com"],
   },
